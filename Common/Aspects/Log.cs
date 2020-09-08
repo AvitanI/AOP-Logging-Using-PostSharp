d0f06389c4d6cf869faad9182f841681ca955af0
AvitanI <Avitanidan@gmail.com>
2020-09-08 17:52:32 +0300

    Add comments.

--- Common/Aspects/Log.cs
+++ Common/Aspects/Log.cs
@@ -7,6 +7,10 @@ namespace Common.Aspects
     [PSerializable]
     public class Log : OnMethodBoundaryAspect
     {
+        /// <summary>
+        /// Method executed before the body of methods to which this aspect is applied.
+        /// </summary>
+        /// <param name="args"></param>
         public override void OnEntry(MethodExecutionArgs args)
         {
             args.MethodExecutionTag = Stopwatch.StartNew();
@@ -14,11 +18,21 @@ namespace Common.Aspects
             Debug.WriteLine($"The {args.Method.Name} method has been entered.");
         }
 
+        /// <summary>
+        /// Method executed after the body of methods to which this aspect is applied, but
+        /// only when the method successfully returns (i.e. when no exception flies out the method.).
+        /// </summary>
+        /// <param name="args"></param>
         public override void OnSuccess(MethodExecutionArgs args)
         {
             Debug.WriteLine($"The {args.Method.Name} method executed successfully.");
         }
 
+        /// <summary>
+        /// Method executed after the body of methods to which this aspect is applied, even
+        /// when the method exists with an exception (this method is invoked from the finally block).
+        /// </summary>
+        /// <param name="args"></param>
         public override void OnExit(MethodExecutionArgs args)
         {
             var sw = (Stopwatch)args.MethodExecutionTag;
@@ -27,6 +41,11 @@ namespace Common.Aspects
             Debug.WriteLine($"The {args.Method.Name} method has exited (total execution time in MS: {sw.ElapsedMilliseconds}).");
         }
 
+        /// <summary>
+        /// Method executed after the body of methods to which this aspect is applied, in
+        /// case that the method resulted with an exception.
+        /// </summary>
+        /// <param name="args"></param>
         public override void OnException(MethodExecutionArgs args)
         {
             Debug.WriteLine($"An exception was thrown in {args.Method.Name}.");
